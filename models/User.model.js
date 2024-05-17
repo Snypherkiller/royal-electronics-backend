2129395ec26601790416ff107471a4324c518650
sakindu rathnasiri <sakindunivein@example.com>
2024-05-17 10:06:28 +0530

    Privileged

--- models/User.model.js
+++ models/User.model.js
@@ -1,24 +1,26 @@
-const mongoose=require("mongoose")
+import mongoose from "mongoose";
 
-const UserSchema=new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        unique:true
+export const UserSchema = new mongoose.Schema({
+    username : {
+        type: String,
+        required : [true, "Please provide unique Username"],
+        unique: [true, "Username Exist"]
     },
-    password:{
-         type:String,
-         required:true   
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        unique : false,
     },
-    userName:{
-        type:String,
-        required:true
+    email: {
+        type: String,
+        required : [true, "Please provide a unique email"],
+        unique: true,
     },
-    userType:{
-        type:String,
-        required:true
-    }
+    firstName: { type: String},
+    lastName: { type: String},
+    mobile : { type : Number},
+    address: { type: String},
+    profile: { type: String}
 });
 
-const User=mongoose.model("User",UserSchema);
-module.exports=User;
+export default mongoose.model.Users || mongoose.model('User', UserSchema);
