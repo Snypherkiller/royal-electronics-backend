11c2d88573f107370a32aee4891edb63587935a6
sakindu rathnasiri <sakindunivein@example.com>
2024-05-17 13:53:22 +0530

    privileged

--- routes/appController.js
+++ routes/appController.js
@@ -39,7 +39,7 @@ export async function register(req,res){
   //console.log('req.body:', req.body);
 
     try {
-        const { username, password, profile, email , firstname , lastname , address , mobile} = req.body;        
+        const { username, password, profile, email} = req.body;        
 
         // check the existing user
         /**const existUsername = new Promise((resolve, reject) => {
